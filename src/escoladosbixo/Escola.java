d4bf86b839461558f3f0bae0eafb9ae1e9f97cf4
jowjow22 <jonathan224santos@gmail.com>
2019-10-03 11:31:32 -0300

    classe escola completa

--- src/escoladosbixo/Escola.java
+++ src/escoladosbixo/Escola.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package escoladosbixo;
 
-/**
- *
- * @author Usuário
- */
+package escoladosbixo;
+import java.util.ArrayList;
+import java.util.List; 
+
+
 public class Escola {
+    private String nomeEscola;
+    private List<Turma> turmas = new ArrayList<Turma>();
+
+    public String getNomeEscola() {
+        return nomeEscola;
+    }
+
+    public void setNome(String nome) {
+        this.nomeEscola = nomeEscola;
+    }
+
+    public void exibirTurma(){
+        for(Turma t : turmas){
+            System.out.println("Nome Turma: "+this.getNomeEscola()+"  Turma: " + t.getNomeTurma());
+    }
+    }
+
+    public void addTurma(Turma t) {
+        turmas.add(t);
+    }
     
 }
